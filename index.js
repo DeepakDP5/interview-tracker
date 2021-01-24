a7ab3349cabaf45236075300dde9b20ac04f2463
SUKHENDU SHARMA <sukhendusharma@Vinta-MacBook-Air.local>
2021-01-24 19:06:49 +0530

    tested api

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const { request } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.json());
 app.get('/',(req,res)=>{
-	res.status(200).json({
-		message:'hello world'
-	})
+	res.status(200).send("<h6>meow</h6>");
 })
+app.post('/',(req,res)=>{
+	console.log(req.body);
+	res.status(200).send("meow");
+})
+
 app.listen(port,()=>{
 	console.log('kuch bhi kuch bhi');
 })
+
